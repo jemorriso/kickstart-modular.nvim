f73d69fa6da1cc774d8d81edff1e77ce1c812bf0
jerry <jeremy.morrison36@gmail.com>
2024-05-05 00:38:14 -0700

    update mappings

--- lua/custom/plugins/copilotchat.lua
+++ lua/custom/plugins/copilotchat.lua
@@ -6,12 +6,13 @@ return {
     { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
   },
   config = function()
-    vim.keymap.set('n', '<leader>ac', function()
+    vim.keymap.set('n', '<leader>cc', function()
       local input = vim.fn.input 'Quick Chat: '
       if input ~= '' then
         require('CopilotChat').ask(input, { selection = require('CopilotChat.select').buffer })
       end
     end)
+    vim.keymap.set('n', '<leader>ct', '<cmd>CopilotChatToggle<cr>')
     require('CopilotChat').setup {}
   end,
 }
