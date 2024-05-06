e06dbc3f4b8e0d166efb0aa6c3dc5ba26dc00e0a
jerry <jeremy.morrison36@gmail.com>
2024-05-05 21:14:39 -0700

    update keybinding

--- lua/custom/plugins/smart-splits.lua
+++ lua/custom/plugins/smart-splits.lua
@@ -8,7 +8,7 @@ return {
     }
   end,
   keys = {
-    { '<localleader>r', [[<cmd>lua require("smart-splits").start_resize_mode()<cr>]], desc = 'enter resize mode' },
+    { ';r', [[<cmd>lua require("smart-splits").start_resize_mode()<cr>]], desc = 'enter resize mode' },
     { '<A-h>', [[<cmd>lua require("smart-splits").move_cursor_left()<cr>]], desc = 'move cursor left' },
     { '<A-j>', [[<cmd>lua require("smart-splits").move_cursor_down()<cr>]], desc = 'move cursor down' },
     { '<A-k>', [[<cmd>lua require("smart-splits").move_cursor_up()<cr>]], desc = 'move cursor up' },
